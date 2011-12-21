ea40f2da2c63a29cb8100af28167165cd81138e1
Estevan Pequeno <estevan.pequeno@gmail.com>
2011-12-21 01:43:14 -0600

    couple more

---
+++ ex04.rb
@@ -0,0 +1,20 @@
+cars = 100
+space_in_a_car = 4.0
+drivers = 30
+passengers = 90
+cars_not_driven = cars - drivers
+cars_driven = drivers
+carpool_capacity = cars_driven * space_in_a_car
+average_passengers_per_car = passengers / cars_driven
+
+puts "There are #{cars} cars available."
+puts "There are only #{drivers} drivers available."
+puts "There will be #{cars_not_driven} empty cars today."
+puts "We can transport #{carpool_capacity} people today."
+puts "We have #{passengers} passengers to carpool today."
+puts "We need to put about #{average_passengers_per_car} in each car."
+
+# Extra credit, if you mistype the variable names at the bottom
+# the interpreter will whine at you because you are trying to 
+# call a variable that is undefined. It'll tell you which line the 
+# mismatch is on.
